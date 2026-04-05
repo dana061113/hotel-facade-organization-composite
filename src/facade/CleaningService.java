c626211132d515616e7dd152038a48825d52bd3c
dana061113 <danakobylandykyzy@gmail.com>
2026-04-05 20:37:34 +0500

    Add CleaningService class for scheduling cleanings

---
+++ src/facade/CleaningService.java
@@ -0,0 +1,12 @@
+package facade;
+
+public class CleaningService {
+
+    public void scheduleCleaning() {
+        System.out.println("Уборка жоспарланды");
+    }
+
+    public void cleanNow() {
+        System.out.println("Уборка орындалды");
+    }
+}
