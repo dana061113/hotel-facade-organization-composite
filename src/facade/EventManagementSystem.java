3456b6e4fad9fbd6ebdca8d302bdc5154e4a4bf9
dana061113 <danakobylandykyzy@gmail.com>
2026-04-05 20:37:04 +0500

    Add EventManagementSystem class for event handling

---
+++ src/facade/EventManagementSystem.java
@@ -0,0 +1,12 @@
+package facade;
+
+public class EventManagementSystem {
+
+    public void bookHall() {
+        System.out.println("Конференц зал брондалды");
+    }
+
+    public void orderEquipment() {
+        System.out.println("Жабдықтар заказ берілді");
+    }
+}
